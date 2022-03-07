37387dd45bd80c1e95a954b20fd3048e23fab9e1
Deepanshu Batra <deepanshu.batra@akalinfosys.com>
2022-03-07 14:10:15 +0530

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Hello {{firstName}} {{getlastName()}} , Welcome to Akal Information</h1><p>Your Roll No : {{rollno}}</p>`,
+  template: `<h1>Hello {{firstName}} {{getlastName()}} , Welcome to Akal Information</h1><p>Your Roll No : {{rollno}}</p><button [disabled]="isButtonDisabled">Submit</button>`,
   styles: []
 })
 export class AppComponent {
   firstName:string="Deepanshu";
   lastname:string="batra";
   rollno:number=21033;
+  isButtonDisabled:boolean=false;
   getlastName(){
     return this.lastname;
   }
