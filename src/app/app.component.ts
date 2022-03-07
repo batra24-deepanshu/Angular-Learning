0b30405c32474e6be90262420a18b11bfd044106
Deepanshu Batra <deepanshu.batra@akalinfosys.com>
2022-03-07 14:01:24 +0530

    string-interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Hello {{firstName}} {{getlastName()}} , Welcome to Akal Information</h1><p>Your Roll No : {{rollno}}</p>`,
+  styles: []
 })
 export class AppComponent {
-  title = 'firstApp';
+  firstName:string="Deepanshu";
+  lastname:string="batra";
+  rollno:number=21033;
+  getlastName(){
+    return this.lastname;
+  }
 }
