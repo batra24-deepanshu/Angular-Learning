5d94d52dcce15f91aa124b33a13432a0974403c5
Deepanshu Batra <deepanshu.batra@akalinfosys.com>
2022-03-07 21:26:06 +0530

    Passing data to event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Hello {{firstName}} {{getlastName()}} , Welcome to Akal Information</h1><p>Your Roll No : {{rollno}}</p><button (click)="toggleButton()" [disabled]="isButtonDisabled">{{isButtonDisabled ?"Enable":"Disable"}}</button><button (click)="enabledButton()">"Enable Button"</button>`,
+  template: `<div>
+    <label>UserName</label>
+    <input (input)="getFirstName($event)"  placeholder="Enter UserName"/>
+    <button (click)="saveUserName()">Save Name</button>
+    <h1>Your Name is : {{displayName}}</h1>
+  </div>`,
   styles: []
 })
 export class AppComponent {
-  firstName:string="Deepanshu";
-  lastname:string="batra";
-  rollno:number=21033;
-  isButtonDisabled:boolean=false;
-  getlastName(){
-    return this.lastname;
-  }
-  toggleButton(){
-    this.isButtonDisabled=!this.isButtonDisabled;
-  }
-  enabledButton(){
-    this.isButtonDisabled=false;
-  }
+  firstName:string="";
+  displayName:string=""
+  
+ getFirstName(data:any){
+   this.firstName=data.target.value;
+ }
+ saveUserName(){
+   this.displayName=this.firstName;
+ }
 }
